dfe3e92653d9c7ebcadb55b3945c618a5a3247af
刘侃 <liukan@JerryMac.local>
2016-12-27 13:57:57 +0800

    add initial value to make it more smart

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -36,22 +36,27 @@ var inputs = {};
 //上银手续费50
 inputs.shangyin_fee.value = 50;
 inputs.haobtc_sell.value = data.haobtc.ticker.sell;
-inputs.shangyin_rate.value = data.shangyin.result.rate * 1.0025;
-inputs.coinbase_rate.value = parseFloat(data.coinbase.datas.ticker.selldollar) + 3;
+inputs.shangyin_rate.value = data.shangyin.result.rate * 1.002;
+inputs.coinbase_rate.value = parseFloat(data.coinbase.datas.ticker.selldollar) + 4;
+inputs.initial_rmb.value = 29990;
 
 function To() {
     
 }
 
-btnCal3.onclick = function () {
+function calculate() {
     var startRMB = inputs.initial_rmb.value;
     var shangyinRate = inputs.shangyin_rate.value;
     var shangyinFee = inputs.shangyin_fee.value;
     var coinbaseRate = inputs.coinbase_rate.value;
     var haobtcSell = inputs.haobtc_sell.value;
 
-    //
+        //
     var gain = (startRMB - shangyinFee - 8 * shangyinRate) * 0.997 * 0.998 * 0.9851 * haobtcSell / (shangyinRate * coinbaseRate);
     inputs.get3.value = gain;
     inputs.get4.value = (gain - startRMB)/startRMB;
-};
+}
+
+btnCal3.onclick = calculate;
+
+calculate();
